37bf01b8cccc84e544c50440db65fcb9cd1a83c7
shinekumar23 <shinekumar23@gmail.com>
2020-08-05 20:37:51 +0530

    Calculated daily Employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -x
+EMP_RATE_PER_HR=20
 Attendence=$((RANDOM%2))
 case $Attendence in
 					0) echo "Employee is present"
+						EmpHrs=8
 						;;
 					1) echo "Employee is absent"
+						EmpHrs=0
 						;;
 esac
+dailyWage=$(($EMP_RATE_PER_HR*$EmpHrs))
+echo "Daily wage of employee is $dailyWage"
+
