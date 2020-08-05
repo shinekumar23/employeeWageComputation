80f9d98c6fdae907df9c791e43a5ea143175578e
shinekumar23 <shinekumar23@gmail.com>
2020-08-05 21:05:20 +0530

    Calculated monthly wage for 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -x
 IS_PART_TIME=1
 IS_FULL_TIME=2
+totalSalary=0
 EMP_RATE_PER_HR=20
-Attendence=$((RANDOM%3))
-case $Attendence in
-						$IS_PART_TIME) EmpHrs=4
-										   ;;
-						$IS_FULL_TIME) EmpHrs=8
-							            ;;
-					      	      *) EmpHrs=0
-										   ;; 
-esac
-dailyWage=$(($EMP_RATE_PER_HR*$EmpHrs))
-echo "Daily wage of employee is $dailyWage"
+numWorkingDays=20
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+                        empCheck=$((RANDOM%3))
+                                         case $empCheck in
+                                                     $IS_PART_TIME) EmpHrs=4
+																						  ;;
+																	  $IS_FULL_TIME) EmpHrs=8
+																						  ;;
+																					  *) EmpHrs=0
+																						  ;;
+													  esac
+                                                        salary=$(($EmpHrs*$EMP_RATE_PER_HR))
+                                                        totalSalary=$(($totalSalary+$salary))
+done
+echo "Total salary is $totalSalary"
+
+
+
 
