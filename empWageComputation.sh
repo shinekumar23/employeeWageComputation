ebee180655922354d24f7060ccef75e9e1b9fa7a
shinekumar23 <shinekumar23@gmail.com>
2020-08-05 22:04:59 +0530

    Added function to get employee hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,24 +8,31 @@ MAX_HRS_IN_MONTH=100
 totalEmpHr=0
 totalWorkingDays=0
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numWorkingDays ]]
+function getWorkHrs() {
+        case $1 in
+                $IS_FULL_TIME)
+                        empHrs=8
+                        ;;
+                $IS_PART_TIME)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+        esac
+}
+
+function getEmpWage () {
+        echo $(($1*$EMP_RATE_PER_HR))
+}
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&   $totalWorkingDays -lt $numWorkingDays ]]
 do
-                        ((totalWorkingDays++))
-                        empCheck=$((RANDOM%3))
-                        case $empCheck in
-                                                $IS_FULL_TIME)
-                                                                        empHrs=8
-                                                                        ;;
-                                                $IS_PART_TIME)
-                                                                        empHrs=4
-                                                                        ;;
-                                                *)
-                                                                        empHrs=0
-                                                                        ;;
-                        esac
-                        totalEmpHr=$(($totalEmpHr+$empHrs))
+        ((totalWorkingDays++))
+        getWorkHrs $((RANDOM%3))
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+        dailyWages[$totalWorkingDays]=$(($empHrs*$EMP_RATE_PER_HR))
 done
-totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR))
+totalSalary="$( getEmpWage $totalEmpHr )"
+
 
 
 
